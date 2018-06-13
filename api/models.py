e70a7e279ae3948aafb78b1ab25ecc0689264a83
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2018-06-13 18:09:29 +0200

    fix typos in models and serializers

--- api/models.py
+++ api/models.py
@@ -85,7 +85,7 @@ class SeFirmware(Resource):
 
 
 class SeFirmwareFinalVersion(Resource):
-    version = models.IntegerField()
+    version = models.CharField(max_length=255)
     display_name = models.CharField(max_length=255, null=True, blank=True)
     notes = models.TextField(blank=True, null=True)
     perso = models.CharField(max_length=255, null=True, blank=True)
@@ -181,7 +181,7 @@ class Application(Resource):
 
 
 class ApplicationVersion(Resource):
-    version = models.IntegerField()
+    version = models.CharField(max_length=255)
     display_name = models.CharField(max_length=255, null=True, blank=True)
     icon = models.CharField(max_length=255, null=True, blank=True)
     notes = models.TextField(blank=True, null=True)
