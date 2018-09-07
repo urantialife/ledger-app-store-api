2916304eaf1c2433c4aa7fbbcdfb905f8dd77a6f
Pierre Pollastri <pierre@ledger.fr>
2018-09-07 11:14:08 +0200

    Add manager.api.live.ledger.com to CORS whitelist

--- ledger_app_store/settings.py
+++ ledger_app_store/settings.py
@@ -141,7 +141,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'localhost:9000',
     '127.0.0.1:9000',
-    'vault.ledger.com'
+    'vault.ledger.com',
+    'manager.api.live.ledger.com'
 )
 
 AWS_REGION = os.environ.get('AWS_REGION')
